dfe6e311d7c64fc8d24e3c0b52aee5c2cf8dca7d
Daniel Earwicker <dan@earwicker.com>
2015-02-20 20:57:49 +0000

    Brought in knockout tests and basic gruntfile to run them

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        build: {
+            main: {}
+        },
+        test: {
+            node: 'spec/runner.node.js'
+        }
+    });
+
+    grunt.registerTask('clean', 'Clean up output files.', function (target) {
+        if (grunt.file.exists('build')) {
+            grunt.file.delete('build');
+        }
+        return !this.errorCount;
+    });
+
+    grunt.registerMultiTask('build', 'Build', function() {
+        if (!this.errorCount) {
+            grunt.file.mkdir('build/output');
+            grunt.file.copy('lib/knockout.clear.js',
+                            'build/output/knockout.clear.js');
+            grunt.file.copy('bower_components/knockout/dist/knockout.debug.js',
+                            'build/output/knockout-latest.js');
+        }
+        return !this.errorCount;
+    });
+
+    grunt.registerMultiTask('test', 'Run tests', function () {
+        var done = this.async();
+        grunt.util.spawn({ cmd: this.target, args: [this.data] },
+            function (error, result, code) {
+                if (code === 127 /*not found*/) {
+                    grunt.verbose.error(result.stderr);
+                    // ignore this error
+                    done(true);
+                } else {
+                    grunt.log.writeln(result.stdout);
+                    if (error)
+                        grunt.log.error(result.stderr);
+                    done(!error);
+                }
+            }
+        );
+    });
+
+    // Default task.
+    grunt.registerTask('default', ['clean', 'build', 'test']);
+};
